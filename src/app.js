bacf6fec5ae1f744d083fa696c509b4df2311020
Gerardo Peralta <peralta.gerardodevelops@gmail.com>
2023-04-21 13:15:08 -0600

    router and controller were descentralizated

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('Soy rama develop');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use('/home', require('./api/routes/homeRoutes'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
