8f2ad86709b9e26da42f4e845a2f02dfbf2c1219
Gerardo Peralta <peralta.gerardodevelops@gmail.com>
2023-04-21 15:31:06 -0600

    cors and helmet was created

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const helmet = require("helmet");
+const cors = require('cors')
 const app = express()
 const port = 3000
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors())
 
 app.use('/home', require('./api/routes/homeRoutes'))
 app.use('/login', require('./api/routes/authRoutes'))
