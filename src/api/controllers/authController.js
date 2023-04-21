454ab4136c75c93bcbfd89ddbc1008c2d2c6972f
Gerardo Peralta <peralta.gerardodevelops@gmail.com>
2023-04-21 14:03:48 -0600

    users and password was created

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -1,10 +1,19 @@
 var jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+const users = {
+    gerardo: 'hola123',
+    gerald: 'adios123'
+}
+
 const loginController = (req, res) => {
+    const { user, password } = req.body;
+
+    if(users[user] !== password) return res.status(401).json({msg: 'bad credentials'});
+
   const myjwt = jwt.sign(
     {
-      data: "foobar",
+      data: {user},
     },
     process.env.JWT_KEY,
     { expiresIn: "1h" }
