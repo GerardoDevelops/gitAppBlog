d4854bbe257ed7da7e6b3ad83575f70f6d8f7a15
Gerardo Peralta <peralta.gerardodevelops@gmail.com>
2023-04-22 10:38:18 -0600

    The create function has been refactored to check if the blog name already exists.

--- src/api/controllers/blogController.js
+++ src/api/controllers/blogController.js
@@ -1,10 +1,14 @@
 const { v4: uuidv4 } = require('uuid');
+const { checkIfIdExist, checkIfNameBlogExist } = require('../../common');
 const blogList = {};
 
 const createController = (req, res) => {
     const { name, tag, description } = req.body;
 
-    if( !name || !tag || !description ) res.status(200).json({ msg: 'Information incomplete' })
+    if( !name || !tag || !description ) res.status(200).json({ msg: 'Information incomplete' });
+
+    const isBlogNameExist = checkIfNameBlogExist(name, blogList);
+    if (isBlogNameExist) return res.status(409).json({msg: 'Nombre no disponible'})
 
     const uuid = uuidv4();
     blogList[uuid] = { name, tag, description };
@@ -18,12 +22,29 @@ const getController = (req, res) => {
 
 const findById = (req, res) => {
     const { id } = req.params;
-    console.log(id);
     res.status(200).json({ blog: { id, ...blogList[id] } })
+}
+
+const update = (req, res) => {
+    const { id } = req.params;
+    const existId = checkIfIdExist(id, blogList);
+
+    if(!existId) return res.status(401).json({msg: `No existe un registro con id ${id}`})
+
+    const { name, tag, description } = req.body;
+    if( !name || !tag || !description ) res.status(200).json({ msg: 'Information incomplete' })
+
+    const isBlogNameExist = checkIfNameBlogExist(name, blogList);
+    if (isBlogNameExist) return res.status(409).json({msg: 'Nombre no disponible'})
+
+    blogList[id] = { name, tag, description };
+
+    res.status(200).json({id, ...blogList[id]})
 }
 
 module.exports = {
     createController,
     getController,
-    findById
+    findById,
+    update
 }
--- src/api/routes/blogRoutes.js
+++ src/api/routes/blogRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createController, getController, findById } = require("../controllers/blogController");
+const { createController, getController, findById, update } = require("../controllers/blogController");
 const router = express.Router();
 
 router.post('/', createController)
 router.get('/', getController)
 router.get('/:id', findById)
+router.put('/:id', update)
 
 module.exports = router
